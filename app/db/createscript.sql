2a297adea9c03fc19d93285c1355df1d608e88dd
ddylvianen <87158436+ddylvianen@users.noreply.github.com>
2025-04-01 12:53:25 +0200

    changed the create script

--- app/db/createscript.sql
+++ app/db/createscript.sql
@@ -298,7 +298,6 @@ VALUES
 
 
  
-
 -- Step: 12
 -- *****************************************************************************************************
 -- Doel : Maak een nieuwe tabel aan met de naam Zangeres
@@ -352,3 +351,60 @@ VALUES
  ,('Taylor Swift', 570,'Verenigde Staten', '+13421 231356', 34, 1, NULL, SYSDATE(6), SYSDATE(6))
  ,('Beyoncé', 420,'Verenigde Staten', '+18723 213481', 41, 1, NULL, SYSDATE(6), SYSDATE(6))
  ,('Jennifer Lopez', 400,'Verenigde Staten', '+16254 751243', 54, 1, NULL, SYSDATE(6), SYSDATE(6));
+
+
+ 
+-- Step: 14
+-- *****************************************************************************************************
+-- Doel : Maak een nieuwe tabel aan met de naam Cyberaanval
+-- *****************************************************************************************************
+-- Versie       Datum           Auteur              Omschrijving
+-- ******       *****           ******              ************
+-- 01           1-04-2025      Arjan de Ruijter    Tabel Cyberaanval
+-- *****************************************************************************************************
+-- Verbeter de dataypen van de onderstaande velden van de tabel Cyberaanval
+-- *****************************************************************************************************
+
+CREATE TABLE Cyberaanval
+(
+     Id                 SMALLINT UNSIGNED    NOT NULL AUTO_INCREMENT,
+     Naam               VARCHAR(250)         NOT NULL,
+     Jaar               SMALLINT UNSIGNED    NOT NULL,
+     AantalSlachtoffers BIGINT UNSIGNED      NOT NULL,
+     TypeAanval         VARCHAR(100)         NOT NULL,
+     Schade             DECIMAL(10,2)        NOT NULL,
+     IsActief           TINYINT(1)           NOT NULL DEFAULT 1,
+     Opmerking          VARCHAR(255)              NULL DEFAULT NULL,
+     DatumAangemaakt    DATETIME             NOT NULL,
+     DatumGewijzigd     DATETIME             NOT NULL,
+     CONSTRAINT PK_Cyberaanval PRIMARY KEY (Id)
+) ENGINE=InnoDB;
+
+
+-- Step: 15
+-- *****************************************************************
+-- Doel : Vul de tabel Cyberaanval met gegevens
+-- *****************************************************************
+-- Versie       Datum           Auteur              Omschrijving
+-- ******       *****           ******              ************
+-- 01           1-4-2025      Arjan de Ruijter     Vulling Cyberaanval
+-- *****************************************************************
+
+INSERT INTO Cyberaanval
+(
+     Naam,
+     Jaar,
+     AantalSlachtoffers,
+     TypeAanval,
+     Schade,
+     IsActief,
+     Opmerking,
+     DatumAangemaakt,
+     DatumGewijzigd
+)
+VALUES
+ ('WannaCry', 2017, 230, 'Ransomware', 4000.00, 1, NULL, NOW(), NOW()),
+ ('Yahoo Datalek', 2013, 3000, 'Datalek', 350.00, 1, NULL, NOW(), NOW()),
+ ('Equifax Hack', 2017, 147, 'Datalek', 700.00, 1, NULL, NOW(), NOW()),
+ ('Target Hack', 2013, 110, 'Creditcardgegevens gestolen', 18.50, 1, NULL, NOW(), NOW()),
+ ('Marriott Hack', 2014, 500, 'Datalek', 100.00, 1, NULL, NOW(), NOW());
